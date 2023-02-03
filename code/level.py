cee7f601fc282fc0847f735ba6fe3c3391aaaf4d
Connor <cpw3352@g.rit.edu>
2023-02-02 23:32:54 -0500

    Added player and rock on initial game load

--- code/level.py
+++ code/level.py
@@ -1,5 +1,7 @@
 import pygame
-
+from settings import *
+from tile import Tile
+from player import Player
 class Level:
     def __init__(self) -> None:
         # get the display surface
@@ -7,6 +9,20 @@ class Level:
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index*TILESIZE
+                y = row_index*TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
+                elif col == 'p':
+                    Player((x,y),[self.visible_sprites])
+                    
+            
+
     def run(self):
         #update and draw game
-        pass
+        self.visible_sprites.draw(self.display_surface)
