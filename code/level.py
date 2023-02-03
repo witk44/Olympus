f79e26d1f9381efef731f75f734c59d6219b1eed
Connor <cpw3352@g.rit.edu>
2023-02-02 23:46:28 -0500

    Added player movement

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 class Level:
     def __init__(self) -> None:
         # get the display surface
@@ -19,10 +20,12 @@ class Level:
                 if col == 'x':
                     Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
                 elif col == 'p':
-                    Player((x,y),[self.visible_sprites])
+                    self.player = Player((x,y),[self.visible_sprites])
                     
             
 
     def run(self):
         #update and draw game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
