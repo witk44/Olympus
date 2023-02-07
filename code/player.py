303bb0b7180ec5574794da8078432c9f9ba93565
witk44 <69919362+witk44@users.noreply.github.com>
2023-02-07 12:46:39 -0500

    New file strcuture for player, player walking animations created but not implemented

--- code/player.py
+++ code/player.py
@@ -4,13 +4,11 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
+        self.image = pygame.image.load('graphics/player/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-26)
-
         self.direction = pygame.math.Vector2()
         self.speed = 5
-
         self.obstacle_sprites = obstacle_sprites
 
     def input(self):
@@ -18,18 +16,14 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_ESCAPE]:
             pygame.quit()
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            pygame.image.load('graphics/test/player.png').convert_alpha()
             self.direction.y = -1
         elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            pygame.image.load('graphics/test/player.png').convert_alpha()
             self.direction.y = 1
         else:
             self.direction.y = 0
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            pygame.image.load('graphics/test/player.png').convert_alpha()
             self.direction.x = -1
         elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            pygame.image.load('graphics/test/player.png').convert_alpha()
             self.direction.x = 1
         else:
             self.direction.x = 0
