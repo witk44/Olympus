74af74004aec904f3ef53f1517f7180db57f21fa
Connor <cpw3352@g.rit.edu>
2023-02-19 20:45:44 -0500

    Tutorial

---
+++ data/tmx/controls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="controls" tilewidth="64" tileheight="64" tilecount="88" columns="9">
+ <image source="../../graphics/controls.png" width="576" height="467"/>
+</tileset>
